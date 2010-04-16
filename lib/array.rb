785649bee48ee6b4eef1aab945142863d0cf352a
codesponge <billy@codesponge.com>
2010-04-16 16:42:40 -0400

    add swap and swap! to array

--- lib/array.rb
+++ lib/array.rb
@@ -1,8 +1,22 @@
 class Array
-  # => rand
+  
   # returns a random element from the array
   def rand
     self[Kernel.rand(self.length)]
   end
   
+  # returns array with the vales at index_a swaped
+  # with index_b
+  def swap(ind1,ind2)
+    ar = self
+    ar[ind1], ar[ind2] = ar[ind2],ar[ind1]
+  end
+
+  # swaps the vale at index_a with index_b
+  # returns the modified self
+  def swap!(ind1,ind2)
+       self[ind1], self[ind2] = self[ind2], self[ind1]
+  self
+  end
+
 end
